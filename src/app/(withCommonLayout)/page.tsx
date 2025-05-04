c08e5d849b5b2074755c135a13f87bf3b76375cd
rezwan2230 <rezwan2230@gmail.com>
2025-05-04 19:16:45 +0600

    added latest post section and create a postServices

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,14 @@
 import Banner from "@/components/banner/Banner";
+import LatestPosts from "@/components/latestPosts/LatestPost";
+import { getAllPosts } from "@/services/postServices";
 
-const HomePage = () => {
+const HomePage = async() => {
+  const posts = await getAllPosts("isr")
+  console.log(posts);
   return (
     <div className="">
       <Banner/>
+      <LatestPosts posts={posts}/>
     </div>
   );
 };
